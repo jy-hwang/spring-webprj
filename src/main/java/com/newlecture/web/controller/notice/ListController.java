bbd871f99f0950b4e445fafe00601c273388cb2b
jyhwang <stratojy@gmail.com>
2025-04-03 07:35:02 +0900

    feat. 서비스 객체 사용하기

--- src/main/java/com/newlecture/web/controller/notice/ListController.java
+++ src/main/java/com/newlecture/web/controller/notice/ListController.java
@@ -1,14 +1,27 @@
 package com.newlecture.web.controller.notice;
 
+import java.util.List;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.Controller;
+import com.newlecture.web.entity.NoticeView;
+import com.newlecture.web.service.NoticeService;
 
 public class ListController implements Controller {
+
+  private NoticeService noticeService;
+
+  public void setNoticeService(NoticeService noticeService) {
+    this.noticeService = noticeService;
+  }
+
   @Override
   public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
     ModelAndView mv = new ModelAndView("notice.list");
+    List<NoticeView> list = noticeService.getNoticeList("title", "", 1);
+    mv.addObject("list", list);
     return mv;
   }
+
 }
