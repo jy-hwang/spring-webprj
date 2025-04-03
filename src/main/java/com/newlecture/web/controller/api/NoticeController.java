fdcf6414a02c7ca2a7673010493af6a4758c2a87
jyhwang <stratojy@gmail.com>
2025-04-04 01:07:46 +0900

    feat. JSON 출력하기

--- src/main/java/com/newlecture/web/controller/api/NoticeController.java
+++ src/main/java/com/newlecture/web/controller/api/NoticeController.java
@@ -1,17 +1,26 @@
 package com.newlecture.web.controller.api;
 
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import com.newlecture.web.entity.NoticeView;
+import com.newlecture.web.service.NoticeService;
 
 @RestController("apiNoticeController")
 @RequestMapping("/api/notice/")
 public class NoticeController {
 
+  @Autowired
+  private NoticeService service;
+
   @GetMapping("list")
-  public String list() {
-    
-    return "공지사항 list";
+  public List<NoticeView> list() {
+    List<NoticeView> list = service.getNoticeList("title", "", 1);
+    return list;
   }
-  
+
+
+
 }
