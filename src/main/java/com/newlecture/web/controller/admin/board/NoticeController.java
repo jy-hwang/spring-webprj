54a112ecbc5834e4a822828db82f5615ca3863e8
jyhwang <stratojy@gmail.com>
2025-04-06 16:58:45 +0900

    feat. POST 입력을 위한 컨트롤러 추가
    1. Admin 컨트롤러 추가
    2. 패키지 세부적으로 구분
    3. 간단히 어떤 메서드가 필요할지 정리

---
+++ src/main/java/com/newlecture/web/controller/admin/board/NoticeController.java
@@ -0,0 +1,36 @@
+package com.newlecture.web.controller.admin.board;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller("adminNoticeController")
+@RequestMapping("/admin/board/notice/")
+public class NoticeController {
+
+  @GetMapping("list")
+  public String list() {
+    return "";
+  }
+  
+  @PostMapping("reg")
+  @ResponseBody
+  public String reg() {
+    return "reg";
+  }
+  
+  @PutMapping("edit")
+  public String edit() {
+    return "";
+  }
+  
+  @DeleteMapping("del")
+  public String del() {
+    return "";
+  }
+  
+}
